008d858c1dad7938172c0c2da3dfd6a1aa26b30d
stephen infanto <infantostephen@gmail.com>
2022-09-08 22:51:15 +0530

    Time: 389 ms (24.96%), Space: 51.2 MB (49.48%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    
+    while(n>1){
+        n=n/3;
+    }
+    return n==1 ? true :false;
+    
+    
+};
