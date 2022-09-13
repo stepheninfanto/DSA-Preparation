2c37e777507a7c26db29ac1b12b148731f18deee
stephen infanto <infantostephen@gmail.com>
2022-09-13 23:09:47 +0530

    Time: 82 ms (61.99%), Space: 41.5 MB (92.95%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -3,16 +3,7 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-    var res ='';
-    for(let i=0; i< address.length;i++){
-        if(address[i]=='.'){
-            res =res +"[";
-            res = res+address[i];
-            res = res+']';
-        }else{
-             res=res+address[i];
-        }       
-    }
-    
-    return res;
+    var defanged ='';
+    defanged = address.replaceAll(".","[.]");    
+    return defanged;
 };
